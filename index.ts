16e3cf0d33c7501d933eb1e36f56db849ebff28e
Chen-Hao-Ping <93893864+Chen-Hao-Ping@users.noreply.github.com>
2023-02-17 15:25:01 +0800

    1 file modified

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import { mergeScan } from 'rxjs/operators';
 
-interface user_interface {//用戶主體
+interface user_interface {
+  //用戶主體
   index: number;
   notify(String): void;
 }
@@ -26,9 +27,9 @@ const usersGroupList = [];
     });
   },
 };*/
-function createUsersGroup(name:String){
+function createUsersGroup(name: String) {
   let UsersGroup = {
-    name:name,
+    name: name,
     userList: [],
     addUser: (user) => {
       UsersGroup.userList.push(user);
@@ -39,18 +40,16 @@ function createUsersGroup(name:String){
         obs.notify(msg);
       });
     },
-  }
+  };
   usersGroupList.push(UsersGroup);
 }
 
-
-function position_name(List,name){
-  let list = List
-    .filter(List => !(List.name).indexOf(name))
+function position_name(List, name) {
+  let list = List.filter((List) => !List.name.indexOf(name));
   return list[0];
 }
 
-function createUser(name:string,id: number) {
+function createUser(name: string, id: number) {
   let user: user_interface = {
     index: id,
     notify: (msg) => {
@@ -64,29 +63,26 @@ function createUser(name:string,id: number) {
     //.filter(bool => bool == true)
   //console.log(userGroup);
   userGroup[0].addUser(user);*/
-  position_name([...usersGroupList],name).addUser(user);
-
+  position_name([...usersGroupList], name).addUser(user);
 }
 
+createUsersGroup('techer1');
+createUsersGroup('techer2');
+createUsersGroup('techer3');
+createUsersGroup('techer4');
+createUsersGroup('techer5');
 
-
-createUsersGroup("techer1");
-createUsersGroup("techer2");
-createUsersGroup("techer3");
-createUsersGroup("techer4");
-createUsersGroup("techer5");
-
-createUser("techer1",10);
-createUser("techer2",20);
-createUser("techer3",30);
-createUser("techer4",40);
-createUser("techer5",50);
-createUser("techer1",11);
-createUser("techer1",12);
-createUser("techer1",13);
-createUser("techer1",14);
-createUser("techer1",15);
-createUser("techer1",16);
+createUser('techer1', 10);
+createUser('techer2', 20);
+createUser('techer3', 30);
+createUser('techer4', 40);
+createUser('techer5', 50);
+createUser('techer1', 11);
+createUser('techer1', 12);
+createUser('techer1', 13);
+createUser('techer1', 14);
+createUser('techer1', 15);
+createUser('techer1', 16);
 
 /*
 getUser(10);
@@ -101,5 +97,6 @@ let userGroup = usersGroupList
     .filter(usersGroup => !(usersGroup.name).indexOf("techer2"))
 userGroup[0].notifyObserver('Hello word');
 */
-position_name([...usersGroupList],"techer1").notifyObserver('第二次測試群體通知');
-
+position_name([...usersGroupList], 'techer2').notifyObserver(
+  '第二次測試群體通知'
+);
